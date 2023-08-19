3272c4b84ed8408bf29b2d451837fd5dc5133ec5
Irakli Dautashvili <idauta@gmail.com>
2023-08-19 22:04:06 +0200

    fix color mode issue

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -8,13 +8,23 @@ const config = {
   useSystemColorMode: false,
 };
 
-const theme = extendTheme({ config });
+const theme = extendTheme({
+  config,
+  components: {
+    Text: {
+      color: '#fcfcfc',
+    },
+    Heading: {
+      color: '#fcfcfc',
+    },
+  },
+});
 
 export const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
     <CacheProvider>
       <ChakraProvider theme={theme} resetCSS>
-        <ColorModeScript initialColorMode={theme.initialColorMode} />
+        <ColorModeScript initialColorMode="dark" />
         {children}
       </ChakraProvider>
     </CacheProvider>
